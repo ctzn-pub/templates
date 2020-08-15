eb816bab3cfdd99d0b064386fd233aac6cc3f775
uhuggins <uhuggins>
2020-08-14 20:42:24 -0400

    adding spiky map2

--- src/components/gallery/SpikeMap.jsx
+++ src/components/gallery/SpikeMap.jsx
@@ -1,51 +1,106 @@
-import React from 'react';
-import { scaleLinear } from 'd3-scale';
-import { extent } from 'd3-array';
+import React, { useRef, useEffect } from 'react';
+import * as d3 from 'd3';
+import * as topojson from 'topojson-client';
+import population from './data/population.json';
 
-function RandomData() {
-  const data = [...Array(100)].map((e, i) => {
-    return {
-      x: Math.random() * 40,
-      y: Math.random() * 40,
-      temparature: Math.random() * 500,
-    };
-  });
-  return data;
-}
+import us from './data/counties-albers-10m.json';
+const format = d3.format(',.0f');
+const spike = (length, width = 7) => `M${-width / 2},0L0,${-length}L${width / 2},0`;
 
-function Scatter() {
-  const data = RandomData(),
-    w = 600,
-    h = 600,
-    margin = {
-      top: 40,
-      bottom: 40,
-      left: 40,
-      right: 40,
-    };
+const path = d3.geoPath();
+const features = new Map(topojson.feature(us, us.objects.counties).features.map(d => [d.id, d]));
 
-  const width = w - margin.right - margin.left,
-    height = h - margin.top - margin.bottom;
+const data = population.slice(1).map(([population, state, county]) => {
+  const id = state + county;
+  const feature = features.get(id);
+  return {
+    id,
+    position: feature && path.centroid(feature),
+    title: feature && feature.properties.name,
+    value: +population,
+  };
+});
 
-  const xScale = scaleLinear()
-    .domain(extent(data, d => d.x))
-    .range([0, width]);
+const length = d3.scaleLinear([0, d3.max(data, d => d.value)], [0, 200]);
 
-  const yScale = scaleLinear()
-    .domain(extent(data, d => d.y))
-    .range([height, 0]);
+function SpikeMap() {
+  const svgRef = useRef();
+  useEffect(() => {
+    const svg = d3.select(svgRef.current);
 
-  const circles = data.map((d, i) => (
-    <circle key={i} r={5} cx={xScale(d.x)} cy={yScale(d.y)} style={{ fill: 'lightblue' }} />
-  ));
+    svg
+      .append('path')
+      .datum(topojson.feature(us, us.objects.nation))
+      .attr('fill', '#e0e0e0')
+      .attr('d', path);
+
+    svg
+      .append('path')
+      .datum(topojson.mesh(us, us.objects.states, (a, b) => a !== b))
+      .attr('fill', 'none')
+      .attr('stroke', 'white')
+      .attr('stroke-linejoin', 'round')
+      .attr('d', path);
+
+    const legend = svg
+      .append('g')
+      .attr('fill', '#777')
+      .attr('text-anchor', 'middle')
+      .attr('font-family', 'sans-serif')
+      .attr('font-size', 10)
+      .selectAll('g')
+      .data(
+        length
+          .ticks(4)
+          .slice(1)
+          .reverse()
+      )
+      .join('g')
+      .attr('transform', (d, i) => `translate(${975 - (i + 1) * 18},590)`);
+
+    legend
+      .append('path')
+      .attr('fill', 'red')
+      .attr('fill-opacity', 0.3)
+      .attr('stroke', 'red')
+      .attr('d', d => spike(length(d)));
+
+    legend
+      .append('text')
+      .attr('dy', '1.3em')
+      .text(length.tickFormat(4, 's'));
+
+    svg
+      .append('g')
+      .attr('fill', 'red')
+      .attr('fill-opacity', 0.3)
+      .attr('stroke', 'red')
+      .selectAll('path')
+      .data(
+        data
+          .filter(d => d.position)
+          .sort(
+            (a, b) =>
+              d3.ascending(a.position[1], b.position[1]) ||
+              d3.ascending(a.position[0], b.position[0])
+          )
+      )
+      .join('path')
+      .attr('transform', d => `translate(${d.position})`)
+      .attr('d', d => spike(length(d.value)))
+      .append('title')
+      .text(
+        d => `${d.title}
+            ${format(d.value)}`
+      );
+    // return svg.node();
+  }, []);
 
   return (
     <div>
-      <svg width={w} height={h}>
-        <g transform={`translate(${margin.left},${margin.top})`}>{circles}</g>
-      </svg>
+      <svg ref={svgRef} width="975" height="610" />
     </div>
   );
 }
 
-export default Scatter;
+export default SpikeMap;
